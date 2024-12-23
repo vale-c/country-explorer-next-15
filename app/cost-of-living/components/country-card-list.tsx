fa9d66875a49eef1cc35d98fa8a8e696cc9706f2
Valentina Calabrese <valentina.calabrese@remote.com>
2024-12-23 16:32:36 +0100

    removed unused index

--- app/cost-of-living/components/country-card-list.tsx
+++ app/cost-of-living/components/country-card-list.tsx
@@ -92,7 +92,7 @@ export default function CountryCardList({
     <>
       <Hero searchCountry={handleSearchQuery} stats={stats} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {data.map(([country, items], index) => {
+        {data.map(([country, items]) => {
           const priorityItems = getPriorityItems(items);
           return (
             <Card
